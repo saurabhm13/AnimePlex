92459a38a33767082cc4d56c635b046c71fa8984
Saurabh <saurabhm1309@gmail.com>
2023-07-12 23:55:08 +0530

    Home page ui created

---
+++ app/src/main/java/com/example/animeplex/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.animeplex.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.animeplex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
